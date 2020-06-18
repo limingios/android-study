e36d1afc9c9a4bfb7fc203f42feef5121399fb2b
liming6 <liming6@asiainfo.com>
2020-06-18 11:45:13 +0800

    PopupWindow,动画设置

--- app/src/main/java/com/idig8/myapplication/DialogActivity.java
+++ app/src/main/java/com/idig8/myapplication/DialogActivity.java
@@ -1,11 +1,21 @@
 package com.idig8.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 public class DialogActivity extends AppCompatActivity {
 
@@ -56,6 +66,67 @@ public class DialogActivity extends AppCompatActivity {
     public void clickshowDialogBtn(View v){
         MyDialog md = new MyDialog(this,R.style.mydialog);
         md.show();
+    }
+
+    // 设置PopupWindow
+    // 1. 实例化对象
+    // 2. 设置
+    // 3. 显示
+    public void clickshowPopupWindowBtn(View v){
+        // 参数1：用在弹窗中的view
+
+        // 当整个这个布局，view来进行处理。findById是内部的一个btn
+
+        View vv = LayoutInflater.from(this).inflate(R.layout.activity_popup,null);
+        //  实例化对象
+        //  参数1：用在弹窗中的view
+        //  参数2，3弹窗的宽高
+        //  参数4（focusable）
+        final PopupWindow window = new PopupWindow(vv,400,100,true);
+
+        // 2. 设置（背景、动画）
+        // 设置背景
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        // 设置能响应外部的点击事件
+        window.setOutsideTouchable(true);
+
+        // 设置能响应点击事件
+        window.setTouchable(true);
+
+        // 创建动画资源，创建一个style应用动画资源，对当前弹窗的动画风格设置
+        window.setAnimationStyle(R.style.translate_anim);
+
+        // 3. 显示
+        // 参数1（anchor）:锚
+        // 参数2，3：相对于描在x，y方向上的偏移量
+        window.showAsDropDown(v,100,50);
+
+        //为弹窗中的文本添加点击事件
+        vv.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DialogActivity.this,"您点击了全选",Toast.LENGTH_SHORT).show();
+                window.dismiss();   //控制弹窗消失
+            }
+        });
+
+        vv.findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DialogActivity.this,"您点击了复制",Toast.LENGTH_SHORT).show();
+                // 控制弹窗消失
+                window.dismiss();
+            }
+        });
+
+        vv.findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DialogActivity.this,"您点击了粘贴",Toast.LENGTH_SHORT).show();
+                window.dismiss();
+            }
+        });
     }
 
 
---
+++ app/src/main/java/com/idig8/myapplication/PopupActivity.java
@@ -0,0 +1,14 @@
+package com.idig8.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PopupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup);
+    }
+}

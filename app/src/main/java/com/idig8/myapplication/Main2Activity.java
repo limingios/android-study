8d10d72be5080eece98dd4dc6819647a25c7bf4d
liming6 <liming6@asiainfo.com>
2020-07-09 17:20:56 +0800

    独立项目的界面开发，底部tab页面的设置

--- app/src/main/java/com/idig8/myapplication/Main2Activity.java
+++ app/src/main/java/com/idig8/myapplication/Main2Activity.java
@@ -3,12 +3,84 @@ package com.idig8.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class Main2Activity extends AppCompatActivity {
+import com.idig8.myapplication.fragment.FindFragment;
+import com.idig8.myapplication.fragment.MainFragment;
+import com.idig8.myapplication.fragment.MeFragment;
+
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+
+    protected LinearLayout mMainLine;
+    protected LinearLayout mFindLine;
+    protected LinearLayout mMeLine;
+    protected MainFragment mMainFragment = new MainFragment(); //首页
+    protected FindFragment mFindFragment = new FindFragment(); //发现
+    protected MeFragment mMeFragment = new MeFragment(); //我的
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        initView();
+        // 获取管理类
+        this.getSupportFragmentManager()
+                .beginTransaction()
+        // 事物添加 默认：显示首页，其他的页面隐藏
+                .add(R.id.container_context,mMainFragment)
+                .add(R.id.container_context,mFindFragment)
+                .hide(mFindFragment)
+                .add(R.id.container_context,mMeFragment)
+                .hide(mMeFragment)
+        // 提交
+        .commit();
+
+    }
+
+    private void initView(){
+        mMainLine = this.findViewById(R.id.menu_main);
+        mFindLine = this.findViewById(R.id.menu_find);
+        mMeLine = this.findViewById(R.id.menu_me);
+        mMainLine.setOnClickListener(this);
+        mFindLine.setOnClickListener(this);
+        mMeLine.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.menu_main://首页
+                // 获取管理类
+                this.getSupportFragmentManager()
+                        .beginTransaction()
+                        // 事物添加 默认：显示首页，其他的页面隐藏
+                        .show(mMainFragment)
+                        .hide(mFindFragment)
+                        .hide(mMeFragment)
+                        // 提交
+                        .commit();
+                break;
+            case R.id.menu_find://发现
+                this.getSupportFragmentManager()
+                        .beginTransaction()
+                        // 事物添加 默认：显示首页，其他的页面隐藏
+                        .hide(mMainFragment)
+                        .show(mFindFragment)
+                        .hide(mMeFragment)
+                        // 提交
+                        .commit();
+                break;
+            case R.id.menu_me://我的
+                this.getSupportFragmentManager()
+                        .beginTransaction()
+                        // 事物添加 默认：显示首页，其他的页面隐藏
+                        .hide(mMainFragment)
+                        .hide(mFindFragment)
+                        .show(mMeFragment)
+                        // 提交
+                        .commit();
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/idig8/myapplication/fragment/MeFragment.java
@@ -0,0 +1,23 @@
+package com.idig8.myapplication.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.idig8.myapplication.R;
+
+/**
+ * 我的
+ */
+public class MeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_me,container,false);
+    }
+}

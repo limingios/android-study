e097774c7e1472f8f368791fdc2e1dd047cc0dde
394498036@qq.com <394498036@qq.com>
2020-07-12 21:24:42 +0800

    UIapp-用户登录界面

--- app/src/main/java/com/idig8/myapplication/fragment/MeFragment.java
+++ app/src/main/java/com/idig8/myapplication/fragment/MeFragment.java
@@ -1,23 +1,43 @@
 package com.idig8.myapplication.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.idig8.myapplication.LoginActivity;
 import com.idig8.myapplication.R;
 
 /**
  * 我的
  */
 public class MeFragment extends Fragment {
+
+    protected Button loginButton;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_me,container,false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        loginButton = (Button)getView().findViewById(R.id.me_header_login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //登录界面
+                Intent login = new Intent(getActivity(), LoginActivity.class);
+                startActivity(login);
+            }
+        });
     }
 }

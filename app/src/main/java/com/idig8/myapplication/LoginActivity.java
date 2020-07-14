a7cb7aa0c631a55bae44a03992bd724e49a0f96e
liming6 <liming6@asiainfo.com>
2020-07-14 20:36:13 +0800

    独立项目的界面开发，发现界面开发

--- app/src/main/java/com/idig8/myapplication/LoginActivity.java
+++ app/src/main/java/com/idig8/myapplication/LoginActivity.java
@@ -2,7 +2,9 @@ package com.idig8.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void toRegister(View w){
+        //注册界面
+        Intent login = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(login);
     }
 }
